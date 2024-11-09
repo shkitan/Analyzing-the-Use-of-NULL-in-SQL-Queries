868d3688c15a3e342d882a9388742db0e5d12755
shkitan <shkitan9@gmail.com>
2024-11-09 21:55:56 +0200

    add display_summary_table_percentage

--- plot_and_analysis.py
+++ plot_and_analysis.py
@@ -11,15 +11,16 @@ df_queries_github_dataset = pd.read_csv('queries_github_dataset.csv')
 
 plt.rcParams['font.size'] = 14
 
-def plot_histogram_for_each_column(df, title):
+def plot_histogram_for_each_column(df, title, color):
     for i, column in enumerate(df.columns[1:], 1):  # Skip query_id
         plt.figure()
-        sns.histplot(df[column])  # You can adjust the number of bins
+        sns.histplot(df[column], color=color)  # You can adjust the number of
+        # bins
         # if title != "banchmark":
         #     plt.title(f'Histogram of {column}: {title} dataset - log scale')
         # else:
         #     plt.title(f'Histogram of {column}: {title} dataset')
-        plt.xlabel(column)
+        plt.xlabel('Complexity According to The Score System')
         plt.ylabel('Frequency')
         if title != "banchmark":
             plt.yscale('log')
@@ -51,7 +52,7 @@ def Plot_incidence_against_complexity(df, title):
         plt.show()
 
 
-def display_summary_table(df):
+def display_summary_table_avg_min_max(df):
     summary_data = {}
 
     for column in df.columns[1:]:  # Skip query_id
@@ -64,9 +65,18 @@ def display_summary_table(df):
     summary_df.index.name = 'Column'
     print(summary_df)
 
+def display_summary_table_percentage(df):
+    Percentage_val = df.apply(lambda column: ((column > 0).sum() /
+                                           column.notna().sum()) * 100)
+    print(Percentage_val)
 
-# plot_histogram_for_each_column(df_benchmark, "banchmark")
-plot_histogram_for_each_column(df_queries_github_dataset, "github queries")
+
+plot_histogram_for_each_column(df_benchmark, "banchmark", 'blue')
+plot_histogram_for_each_column(df_queries_github_dataset, "github queries",
+                               'orange')
 # display_summary_table(df_queries_github_dataset)
 # display_summary_table(df_benchmark)
 # Plot_incidence_against_complexity(df_queries_github_dataset, "github")
+
+# display_summary_table_percentage(df_benchmark)
+# display_summary_table_percentage(df_queries_github_dataset)
